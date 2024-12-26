18ac6be68c00c99c87e808c68aae6fe0fe0223e6
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2024-12-26 14:37:52 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['title' => 'About Page', 'name' => 'Laravel']);
+});
+
+Route::get('/blog', function () {
+    return view('blog', ['title' => 'Blog']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact Page']);
+});
